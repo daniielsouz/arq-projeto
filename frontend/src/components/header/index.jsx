1a6fcb04deeecbd5e95292bb913809a0051c999b
Daniel Souza <danielsouza.dev@gmail.com>
2025-06-02 19:14:59 -0300

    Altera configuração css do Header

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,19 +1,19 @@
 import Logo from '../../img/LogoHeader.png'
-import './header.module.css';
+import style from './header.module.css';
 import { Link } from 'react-router-dom';
 
 
 function Header() {
   return (
-        <header>
-                <img src={Logo} alt='Logo Arq - Janaina Possamai' />
-                <p className='textos'>CAU A309239-9</p>
+        <header className={style.headerContainer}>
+                <img className={style.headerImg} src={Logo} alt='Logo Arq - Janaina Possamai' />
+                <p  className={style.headerText}>CAU A309239-9</p>
             
             <nav>
-                <ul>
-                    <li><Link  to="/">Página Inicial</Link></li>
-                    <li><Link to="/Sobre">Sobre Mim</Link></li>
-                    <li><Link to="/portifolio">Portifólio</Link></li>
+                <ul className={style.headerList}>
+                    <li className={style.headerListItens}><Link className={style.headerLinks} to="/">Página Inicial</Link></li>
+                    <li className={style.headerListItens}><Link className={style.headerLinks} to="/Sobre">Sobre Mim</Link></li>
+                    <li className={style.headerListItens}><Link className={style.headerLinks} to="/portifolio">Portifólio</Link></li>
                 </ul>
             </nav>
         </header> 
